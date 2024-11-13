f661fd6514e334c026f40fe74bdbf9a7491d3364
hyunjo325 <hyunjo325@naver.com>
2024-11-13 17:22:02 +0900

    feat: 그리기 기능 구현

---
+++ src/server/LineInfo.java
@@ -0,0 +1,28 @@
+package server;
+
+import java.awt.*;
+
+public class LineInfo {
+    private Point p;
+    private int color;
+    public LineInfo(Point input, int selectedColor){
+        this.p = input;
+        this.color = selectedColor;
+    }
+    public double getX(){
+        return p.getX();
+    }
+    public double getY(){
+        return p.getY();
+    }
+    public Color getColor(){
+        Color[] colors = {Color.RED, Color.BLUE, Color.GREEN, Color.YELLOW, Color.ORANGE,
+                Color.PINK, Color.MAGENTA, Color.CYAN, Color.WHITE, Color.GRAY, Color.BLACK};
+        return colors[color];
+    }
+    public String getInfo(){
+        String str = "";
+        str += p.getX()+"#"+p.getY()+"#"+color;
+        return str;
+    }
+}
